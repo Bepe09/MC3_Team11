cdf15485ee94b92251b9b5538b5e2ca38ef30c2a
Nam Do-San <75228965+Bepe09@users.noreply.github.com>
2023-08-07 13:46:34 +0700

    Add files via upload
    Upload Xcode File

---
+++ MC3_Team11/MC3_Team11/MC3_Team11App.swift
@@ -0,0 +1,17 @@
+//
+//  MC3_Team11App.swift
+//  MC3_Team11
+//
+//  Created by Brian Putrantio on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MC3_Team11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
